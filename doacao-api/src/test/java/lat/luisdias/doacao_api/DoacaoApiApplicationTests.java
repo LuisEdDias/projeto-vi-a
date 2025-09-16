30e2052c45bae171b4fbe05d4e2564e2a7cee70b
LuisEdDias <edu_vahl@outlook.com>
2025-09-16 20:26:31 -0300

    First commit

---
+++ doacao-api/src/test/java/lat/luisdias/doacao_api/DoacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lat.luisdias.doacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
