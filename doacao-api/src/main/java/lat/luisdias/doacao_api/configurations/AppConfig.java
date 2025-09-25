634f4de1457dd290aacb4f4717ce46f6e752e03c
LuisEdDias <edu_vahl@outlook.com>
2025-09-24 22:00:34 -0300

    CORS policy added

--- doacao-api/src/main/java/lat/luisdias/doacao_api/configurations/AppConfig.java
+++ doacao-api/src/main/java/lat/luisdias/doacao_api/configurations/AppConfig.java
@@ -3,11 +3,26 @@ package lat.luisdias.doacao_api.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
     @Bean
     public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
         return new HiddenHttpMethodFilter();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
